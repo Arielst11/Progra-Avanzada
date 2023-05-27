1c7cbbdb8056d23523ed07768435e5e730d3ef25
Arielst11 <asancheztroz@gmail.com>
2023-05-26 21:01:18 -0600

    inicio del proyecto
    Solo tiene un index y la plantilla funciona

---
+++ Proyecto Grupo 6/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Grupo_6.Controllers
+{
+    public class StoreController : Controller
+    {
+
+
+        public ActionResult index()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("index");
+        }
+
+
+        /*
+        public ActionResult contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("contact");
+        }
+       */
+    }
+}
